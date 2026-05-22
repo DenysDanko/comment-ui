4141bfe1bad23b741aea1ed94fcc0852b7ef8800
Denys <dankosickach@gmail.com>
2026-05-22 12:52:05 +0300

    9 - Prepare to publish

--- src/funcs/constants.js
+++ src/funcs/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:5161";
+export const API_BASE_URL = process.env.REACT_APP_API_URL;
 export const API_ENDPOINTS = {
   captcha: `${API_BASE_URL}/api/captcha`,
   comments: `${API_BASE_URL}/api/comments`,
